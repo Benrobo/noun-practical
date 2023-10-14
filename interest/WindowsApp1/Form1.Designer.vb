9505949b811eb10ad37cd1c3ecd67c6bd74a63ca
benrobo <alumonabenaiah71@gmail.com>
2023-10-13 17:28:30 -1200

    added salary calculator

---
+++ interest/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,355 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grosspayBtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.basicSalaryInp = New System.Windows.Forms.TextBox()
+        Me.clearBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tfairInp = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.rentInp = New System.Windows.Forms.TextBox()
+        Me.hazardInp = New System.Windows.Forms.TextBox()
+        Me.allowancesTotalInp = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.pensionInp = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.nhisInp = New System.Windows.Forms.TextBox()
+        Me.taxInp = New System.Windows.Forms.TextBox()
+        Me.deductionTotalInp = New System.Windows.Forms.TextBox()
+        Me.netpayBtn = New System.Windows.Forms.Button()
+        Me.grosspayInp = New System.Windows.Forms.TextBox()
+        Me.netpayInp = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'grosspayBtn
+        '
+        Me.grosspayBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grosspayBtn.Location = New System.Drawing.Point(99, 315)
+        Me.grosspayBtn.Name = "grosspayBtn"
+        Me.grosspayBtn.Size = New System.Drawing.Size(142, 48)
+        Me.grosspayBtn.TabIndex = 0
+        Me.grosspayBtn.Text = "Gross Pay"
+        Me.grosspayBtn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Basic Salary"
+        '
+        'basicSalaryInp
+        '
+        Me.basicSalaryInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.basicSalaryInp.Location = New System.Drawing.Point(114, 25)
+        Me.basicSalaryInp.Name = "basicSalaryInp"
+        Me.basicSalaryInp.Size = New System.Drawing.Size(153, 29)
+        Me.basicSalaryInp.TabIndex = 2
+        '
+        'clearBtn
+        '
+        Me.clearBtn.Location = New System.Drawing.Point(520, 17)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(128, 37)
+        Me.clearBtn.TabIndex = 3
+        Me.clearBtn.Text = "Clear"
+        Me.clearBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 24)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Allowances"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(21, 133)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 18)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Transport"
+        '
+        'tfairInp
+        '
+        Me.tfairInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tfairInp.Location = New System.Drawing.Point(99, 126)
+        Me.tfairInp.Name = "tfairInp"
+        Me.tfairInp.Size = New System.Drawing.Size(153, 29)
+        Me.tfairInp.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(23, 175)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 18)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Rent"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(23, 217)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(56, 18)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Hazard"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(23, 254)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(46, 18)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Total"
+        '
+        'rentInp
+        '
+        Me.rentInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rentInp.Location = New System.Drawing.Point(99, 168)
+        Me.rentInp.Name = "rentInp"
+        Me.rentInp.Size = New System.Drawing.Size(153, 29)
+        Me.rentInp.TabIndex = 10
+        '
+        'hazardInp
+        '
+        Me.hazardInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hazardInp.Location = New System.Drawing.Point(99, 210)
+        Me.hazardInp.Name = "hazardInp"
+        Me.hazardInp.Size = New System.Drawing.Size(153, 29)
+        Me.hazardInp.TabIndex = 11
+        '
+        'allowancesTotalInp
+        '
+        Me.allowancesTotalInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.allowancesTotalInp.Location = New System.Drawing.Point(99, 254)
+        Me.allowancesTotalInp.Name = "allowancesTotalInp"
+        Me.allowancesTotalInp.Size = New System.Drawing.Size(153, 29)
+        Me.allowancesTotalInp.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(516, 90)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(105, 24)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Deduction"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(306, 185)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 18)
+        Me.Label8.TabIndex = 14
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(408, 210)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(32, 18)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "Tax"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(408, 175)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(43, 18)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "NHIS"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(408, 126)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(62, 18)
+        Me.Label11.TabIndex = 17
+        Me.Label11.Text = "Pension"
+        '
+        'pensionInp
+        '
+        Me.pensionInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.pensionInp.Location = New System.Drawing.Point(495, 122)
+        Me.pensionInp.Name = "pensionInp"
+        Me.pensionInp.Size = New System.Drawing.Size(153, 29)
+        Me.pensionInp.TabIndex = 18
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(408, 254)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(46, 18)
+        Me.Label12.TabIndex = 19
+        Me.Label12.Text = "Total"
+        '
+        'nhisInp
+        '
+        Me.nhisInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nhisInp.Location = New System.Drawing.Point(495, 168)
+        Me.nhisInp.Name = "nhisInp"
+        Me.nhisInp.Size = New System.Drawing.Size(153, 29)
+        Me.nhisInp.TabIndex = 20
+        '
+        'taxInp
+        '
+        Me.taxInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.taxInp.Location = New System.Drawing.Point(495, 210)
+        Me.taxInp.Name = "taxInp"
+        Me.taxInp.Size = New System.Drawing.Size(153, 29)
+        Me.taxInp.TabIndex = 21
+        '
+        'deductionTotalInp
+        '
+        Me.deductionTotalInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.deductionTotalInp.Location = New System.Drawing.Point(495, 254)
+        Me.deductionTotalInp.Name = "deductionTotalInp"
+        Me.deductionTotalInp.Size = New System.Drawing.Size(153, 29)
+        Me.deductionTotalInp.TabIndex = 22
+        '
+        'netpayBtn
+        '
+        Me.netpayBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.netpayBtn.Location = New System.Drawing.Point(99, 369)
+        Me.netpayBtn.Name = "netpayBtn"
+        Me.netpayBtn.Size = New System.Drawing.Size(142, 48)
+        Me.netpayBtn.TabIndex = 23
+        Me.netpayBtn.Text = "Netpay"
+        Me.netpayBtn.UseVisualStyleBackColor = True
+        '
+        'grosspayInp
+        '
+        Me.grosspayInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grosspayInp.Location = New System.Drawing.Point(258, 319)
+        Me.grosspayInp.Name = "grosspayInp"
+        Me.grosspayInp.ReadOnly = True
+        Me.grosspayInp.Size = New System.Drawing.Size(153, 35)
+        Me.grosspayInp.TabIndex = 24
+        '
+        'netpayInp
+        '
+        Me.netpayInp.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.netpayInp.Location = New System.Drawing.Point(258, 373)
+        Me.netpayInp.Name = "netpayInp"
+        Me.netpayInp.ReadOnly = True
+        Me.netpayInp.Size = New System.Drawing.Size(153, 35)
+        Me.netpayInp.TabIndex = 25
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(685, 451)
+        Me.Controls.Add(Me.netpayInp)
+        Me.Controls.Add(Me.grosspayInp)
+        Me.Controls.Add(Me.netpayBtn)
+        Me.Controls.Add(Me.deductionTotalInp)
+        Me.Controls.Add(Me.taxInp)
+        Me.Controls.Add(Me.nhisInp)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.pensionInp)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.allowancesTotalInp)
+        Me.Controls.Add(Me.hazardInp)
+        Me.Controls.Add(Me.rentInp)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.tfairInp)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.clearBtn)
+        Me.Controls.Add(Me.basicSalaryInp)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.grosspayBtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents grosspayBtn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents basicSalaryInp As TextBox
+    Friend WithEvents clearBtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents tfairInp As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents rentInp As TextBox
+    Friend WithEvents hazardInp As TextBox
+    Friend WithEvents allowancesTotalInp As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents pensionInp As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents nhisInp As TextBox
+    Friend WithEvents taxInp As TextBox
+    Friend WithEvents deductionTotalInp As TextBox
+    Friend WithEvents netpayBtn As Button
+    Friend WithEvents grosspayInp As TextBox
+    Friend WithEvents netpayInp As TextBox
+End Class
---
+++ interest/WindowsApp1/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+    Dim basicSalary As Double
+    Dim tfair As Double
+    Dim rent As Double
+    Dim hazard As Double
+    Dim pension As Double
+    Dim nhis As Double
+    Dim tax As Double
+    Dim Total_allowance As Double
+    Dim Total_deduction As Double
+
+    Dim allowancesTotal As Double
+    Dim deductionTotal As Double
+    Dim grossPay As Double
+    Dim netPay As Double
+
+    ' Function to calculate a percentage of a value
+    Private Function CalculatePercentage(percentage As Double) As Double
+        Return (percentage * basicSalary) / 100
+    End Function
+
+    Private Sub ValidateAndAssign(inputField As TextBox, ByRef variable As Double)
+        If IsNumeric(inputField.Text) Then
+            variable = CDbl(inputField.Text)
+        Else
+            ' Handle non-numeric input (e.g., display an error message or clear the field)
+            ' Here, we set the variable to 0 as a default value
+            variable = 0
+        End If
+    End Sub
+
+
+    Private Sub basicSalaryInp_TextChanged(sender As Object, e As EventArgs) Handles basicSalaryInp.TextChanged
+        ValidateAndAssign(basicSalaryInp, basicSalary)
+    End Sub
+
+    Private Sub tfairInp_TextChanged(sender As Object, e As EventArgs) Handles tfairInp.TextChanged
+        ValidateAndAssign(tfairInp, tfair)
+    End Sub
+
+    Private Sub rentInp_TextChanged(sender As Object, e As EventArgs) Handles rentInp.TextChanged
+        ValidateAndAssign(rentInp, rent)
+    End Sub
+
+    Private Sub hazardInp_TextChanged(sender As Object, e As EventArgs) Handles hazardInp.TextChanged
+        ValidateAndAssign(hazardInp, hazard)
+    End Sub
+
+    Private Sub allowancesTotalInp_TextChanged(sender As Object, e As EventArgs) Handles allowancesTotalInp.TextChanged
+
+    End Sub
+
+    Private Sub pensionInp_TextChanged(sender As Object, e As EventArgs) Handles pensionInp.TextChanged
+        ValidateAndAssign(pensionInp, pension)
+    End Sub
+
+    Private Sub nhisInp_TextChanged(sender As Object, e As EventArgs) Handles nhisInp.TextChanged
+        ValidateAndAssign(nhisInp, nhis)
+    End Sub
+
+    Private Sub taxInp_TextChanged(sender As Object, e As EventArgs) Handles taxInp.TextChanged
+        ValidateAndAssign(taxInp, tax)
+    End Sub
+
+    Private Sub deductionTotalInp_TextChanged(sender As Object, e As EventArgs) Handles deductionTotalInp.TextChanged
+
+    End Sub
+
+    Private Sub grosspayBtn_Click(sender As Object, e As EventArgs) Handles grosspayBtn.Click
+        ' Calculate gross pay based on percentages
+        Total_allowance = CalculatePercentage(10) + CalculatePercentage(15) + CalculatePercentage(6.5)
+        allowancesTotalInp.Text = Total_allowance
+        grossPay = basicSalary + Total_allowance
+        grosspayInp.Text = grossPay
+    End Sub
+
+    Private Sub netpayBtn_Click(sender As Object, e As EventArgs) Handles netpayBtn.Click
+        ' Calculate net pay based on percentages
+        Total_deduction = CalculatePercentage(7.5) + CalculatePercentage(5) + CalculatePercentage(2)
+        netPay = grossPay - Total_deduction
+        deductionTotalInp.Text = Total_deduction
+        netpayInp.Text = netPay
+    End Sub
+
+    Private Sub clearBtn_Click(sender As Object, e As EventArgs) Handles clearBtn.Click
+        ' Clear all input fields and result fields
+        basicSalaryInp.Clear()
+        tfairInp.Clear()
+        rentInp.Clear()
+        hazardInp.Clear()
+        allowancesTotalInp.Clear()
+        pensionInp.Clear()
+        nhisInp.Clear()
+        taxInp.Clear()
+        deductionTotalInp.Clear()
+        grosspayInp.Clear()
+        netpayInp.Clear()
+    End Sub
+End Class
