d94c3add7f7017e572cbea184bfc1485cfe893d9
benrobo <alumonabenaiah71@gmail.com>
2023-10-13 19:50:59 -1200

    added project estimation

---
+++ membership/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.charge = New System.Windows.Forms.TextBox()
+        Me.fee = New System.Windows.Forms.TextBox()
+        Me.output = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(27, 90)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(154, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Membership Charges"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(27, 184)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Fee ( % )"
+        '
+        'charge
+        '
+        Me.charge.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.charge.Location = New System.Drawing.Point(30, 124)
+        Me.charge.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.charge.Name = "charge"
+        Me.charge.Size = New System.Drawing.Size(191, 40)
+        Me.charge.TabIndex = 2
+        '
+        'fee
+        '
+        Me.fee.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.fee.Location = New System.Drawing.Point(30, 212)
+        Me.fee.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.fee.Name = "fee"
+        Me.fee.Size = New System.Drawing.Size(191, 40)
+        Me.fee.TabIndex = 3
+        '
+        'output
+        '
+        Me.output.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.output.Location = New System.Drawing.Point(246, 90)
+        Me.output.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.output.Name = "output"
+        Me.output.Size = New System.Drawing.Size(254, 251)
+        Me.output.TabIndex = 4
+        Me.output.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button1.Location = New System.Drawing.Point(31, 272)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(190, 68)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Estimate Projects"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(243, 52)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(183, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Project Estimation ( 6yrs )"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(527, 372)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.output)
+        Me.Controls.Add(Me.fee)
+        Me.Controls.Add(Me.charge)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents charge As TextBox
+    Friend WithEvents fee As TextBox
+    Friend WithEvents output As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ membership/WindowsApp1/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Dim initialFee As Double = 25000
+    Dim feeVal As Double = 4
+    Dim outputVal As Double
+    Dim currentFee As Double
+
+
+    ' assign value to text box on initial render
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        fee.Text = feeVal
+        charge.Text = initialFee
+
+    End Sub
+
+    Private Sub charge_TextChanged(sender As Object, e As EventArgs) Handles charge.TextChanged
+        If IsNumeric(charge.Text) Then
+            initialFee = CDbl(charge.Text)
+        Else
+            initialFee = 0
+        End If
+    End Sub
+
+    Private Sub fee_TextChanged(sender As Object, e As EventArgs) Handles fee.TextChanged
+        If IsNumeric(fee.Text) Then
+            feeVal = CDbl(fee.Text)
+        Else
+            feeVal = 0
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        output.Text = ""
+        For i = 1 To 6
+            ' Concatenate items within the loop
+            initialFee += (feeVal * initialFee) / 100
+            Dim finalVal = "Year " & i & " " & "N" & initialFee.ToString("F1") & vbCrLf & vbCrLf
+            output.Text += finalVal
+        Next i
+    End Sub
+
+End Class
